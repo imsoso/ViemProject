cf56617be22163a14b8403f465919ad80aa256ac
Soso <husosong@163.com>
2024-10-21 16:19:18 +0800

    Feature: Add read owner of function

--- ReadNFT.ts
+++ ReadNFT.ts
@@ -1,14 +1,20 @@
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem/chains'
- 
-const client = createPublicClient({ 
-  chain: mainnet, 
-  transport: http(), 
-}) 
+import { createPublicClient, http, parseEther } from "viem";
+import { mainnet } from "viem/chains";
+import { abi, address } from "./abi";
 
-async function main() {
-  const blockNumber = await client.getBlockNumber();
-  console.log(blockNumber);
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
+
+const tokenId = 114;
+async function NFTOwner() {
+  const owner = await client.readContract({
+    address,
+    abi,
+    functionName: "ownerOf",
+    args: [BigInt(tokenId)],
+  });
+
+  console.log(`The owner of NFT ${tokenId} is ${owner}`);
 }
-
-main();
