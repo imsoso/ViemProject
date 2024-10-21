42d63ef2d228fae80dc23aa1454d50bb9fee0c2e
Soso <husosong@163.com>
2024-10-21 16:18:02 +0800

    Feature: Rename file

--- ReadNFT.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var viem_1 = require("viem");
-var chains_1 = require("viem/chains");
-var client = (0, viem_1.createPublicClient)({
-    chain: chains_1.mainnet,
-    transport: (0, viem_1.http)(),
-});
-//# sourceMappingURL=ReadNFT.js.map
